ae9898338946b6fb98d678c2f04b1660824f5528
choll <c.holloway@gmail.com>
2021-12-26 01:04:07 +0000

    add xtr

---
+++ benchmarks/call_site_latency/benchmark_xtr_call_site_latency.cpp
@@ -0,0 +1,55 @@
+#include "call_site_latency_bench.h"
+
+#include <xtr/logger.hpp>
+
+#include <string>
+
+/***/
+void xtr_benchmark(std::vector<int32_t> thread_count_array, size_t num_iterations_per_thread)
+{
+  std::remove("xtr_call_site_latency_percentile_linux_benchmark.log");
+
+  xtr::logger log("benchmark_xtr_call_site_latency_percentile_linux_benchmark.log");
+
+  set_pthread_affinity(log.consumer_thread_native_handle(), 0);
+
+  // wait for the backend thread to start
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // TODO, add function to setup tsc:
+  // xtr::initialize_tsc();
+  auto s = log.get_sink("main");
+  XTR_LOG_TSC(s, "");
+  s.sync();
+
+  // Define a logging lambda
+#ifdef BENCH_INT_INT_DOUBLE
+  auto log_func = [&log](uint64_t i, uint64_t j, double d) mutable {
+    thread_local auto sink = log.get_sink("main");
+    //XTR_LOG(sink, "Logging int: {}, int: {}, double: {}", i, j, d);
+    //XTR_TRY_LOG_TSC(sink, "Logging int: {}, int: {}, double: {}", i, j, d);
+    //XTR_TRY_LOGL(info, sink, "Logging int: {}, int: {}, double: {}", i, j, d);
+    //XTR_TRY_LOGL_TSC(info, sink, "Logging int: {}, int: {}, double: {}", i, j, d);
+    XTR_LOGL_TSC(info, sink, "Logging int: {}, int: {}, double: {}", i, j, d);
+  };
+#elif defined(BENCH_INT_INT_LARGESTR)
+  auto log_func = [&log](uint64_t i, uint64_t j, std::string const& s) mutable {
+    thread_local auto sink = log.get_sink("main");
+    XTR_LOGL_TSC(info, sink, "Logging int: {}, int: {}, string: {}", i, j, s);
+  };
+#endif
+
+  auto on_start = []() {};
+
+  auto on_exit = []() {};
+
+  // Run the benchmark for n threads
+  for (auto thread_count : thread_count_array)
+  {
+    run_benchmark("Logger: XTR - Benchmark: Caller Thread Latency", thread_count,
+                  num_iterations_per_thread, on_start, log_func, on_exit);
+  }
+}
+/***/
+
+int main(int argc, char* argv[]) { xtr_benchmark(THREAD_LIST_COUNT, ITERATIONS); }
--- run_all.py
+++ run_all.py
@@ -1,26 +1,27 @@
+#!/usr/bin/python
+
 import subprocess
 
-root_dir = ""
+root_dir = "build/benchmarks/call_site_latency/"
 
-benchmark_quill_no_dual_mode_unbounded = root_dir + "benchmark_quill_no_dual_mode_unbounded_call_site_latency"
-benchmark_quill_dual_mode_unbounded = root_dir + "benchmark_quill_dual_mode_unbounded_call_site_latency"
-benchmark_quill_dual_mode_bounded = root_dir + "benchmark_quill_dual_mode_bounded_call_site_latency"
-benchmark_g3log = root_dir + "benchmark_g3log_call_site_latency"
-benchmark_iyengar_nanolog = root_dir + "benchmark_iyengar_nanolog_call_site_latency"
-benchmark_ms_binlog = root_dir + "benchmark_ms_binlog_call_site_latency"
-benchmark_platformlab = root_dir + "benchmark_platformlab_call_site_latency"
-benchmark_reckless = root_dir + "benchmark_reckless_call_site_latency"
-benchmark_spdlog = root_dir + "benchmark_spdlog_call_site_latency"
+benchmark_quill_no_dual_mode_unbounded = "benchmark_quill_no_dual_mode_unbounded_call_site_latency"
+benchmark_quill_dual_mode_unbounded = "benchmark_quill_dual_mode_unbounded_call_site_latency"
+benchmark_quill_dual_mode_bounded = "benchmark_quill_dual_mode_bounded_call_site_latency"
+benchmark_g3log = "benchmark_g3log_call_site_latency"
+benchmark_iyengar_nanolog = "benchmark_iyengar_nanolog_call_site_latency"
+benchmark_ms_binlog = "benchmark_ms_binlog_call_site_latency"
+benchmark_platformlab = "benchmark_platformlab_call_site_latency"
+benchmark_reckless = "benchmark_reckless_call_site_latency"
+benchmark_spdlog = "benchmark_spdlog_call_site_latency"
+benchmark_xtr = "benchmark_xtr_call_site_latency"
 
-benchmarks = [benchmark_quill_no_dual_mode_unbounded, benchmark_quill_dual_mode_unbounded, benchmark_quill_dual_mode_bounded, benchmark_platformlab, benchmark_ms_binlog, benchmark_spdlog, benchmark_g3log, benchmark_iyengar_nanolog, benchmark_reckless]
+benchmarks = [benchmark_quill_no_dual_mode_unbounded, benchmark_quill_dual_mode_unbounded, benchmark_quill_dual_mode_bounded, benchmark_platformlab, benchmark_ms_binlog, benchmark_spdlog, benchmark_g3log, benchmark_iyengar_nanolog, benchmark_reckless, benchmark_xtr]
 
-i = 0
-for bench in benchmarks:
-  s = "bench_results_{}.txt".format(i)
-  print("Writing to {} for {}".format(s, bench))
-  output = open(s, "w")
-  i = i + 1
+for benchname in benchmarks:
+    s = "bench_results_{}.txt".format(benchname)
+    bench = root_dir + benchname
+    print("Writing to {} for {}".format(s, bench))
+    output = open(s, "w")
 
-  for x in range(4):
     print("Running {}".format(bench))
     subprocess.call(bench, stdout=output)
